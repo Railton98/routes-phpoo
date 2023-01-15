858cf2ca0fd7fd444fe08d969892465ab4bdcb70
Railton98 <railtonleal98@gmail.com>
2023-01-14 23:46:48 -0300

    feat: lesson #03 - getting the exact routes

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,10 +1,14 @@
 <?php
 
-namespace App\Core;
+namespace app\core;
 
 class Router
 {
     public static function run()
     {
+        $routerRegistered = new RoutesFilter;
+        $router = $routerRegistered->get();
+
+        dd($router);
     }
 }
--- app/routes/Routes.php
+++ app/routes/Routes.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Routes;
+namespace app\routes;
 
 class Routes
 {
