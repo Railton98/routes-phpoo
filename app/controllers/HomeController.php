1372be076d0dba29de6a35d6befb2eb324b5d781
Railton98 <railtonleal98@gmail.com>
2023-01-18 01:00:16 -0300

    feat: lesson #12 - create database connection | with necessary adjusts

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,10 +2,15 @@
 
 namespace app\controllers;
 
+use app\database\models\User;
+
 class HomeController extends Controller
 {
     public function index()
     {
+        $user = new User;
+        $user->fetch();
+
         $title = 'Home';
 
         $this->view('home', compact('title'));
---
+++ app/database/models/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\database\models;
+
+use app\database\Connection;
+
+class User
+{
+    public function fetch()
+    {
+        $connection = Connection::connect();
+        dd($connection);
+    }
+}
