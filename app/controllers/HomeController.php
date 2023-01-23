f5f39625cbb7cb531c3b9a387d9a3022a6908fe3
Railton98 <railtonleal98@gmail.com>
2023-01-22 22:30:23 -0300

    feat: lesson #22 - create pagination (part 02)

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,24 +2,10 @@
 
 namespace app\controllers;
 
-use app\database\Filters;
-use app\database\models\User;
-
 class HomeController extends Controller
 {
     public function index()
     {
-        $user = new User;
-        $updated = $user->update('id', 3, ['email' => 'railton@tecks.dev']);
-        dd($updated);
-        // $created = $user->create([
-        //     'firstName' => 'Tecks',
-        //     'lastName' => 'Dev',
-        //     'email' => 'tecks@dev.com',
-        //     'password' => '123',
-        // ]);
-        // dd($created);
-
         $title = 'Home';
 
         $this->view('home', compact('title'));
