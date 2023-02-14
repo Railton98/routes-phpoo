e4f5b8ea9da8f925fea84c2cb2bb18fed885d7cf
Railton98 <railtonleal98@gmail.com>
2023-02-13 23:02:37 -0300

    feat: lesson #28 - how create flash messages with PHP

--- app/views/user.php
+++ app/views/user.php
@@ -1,13 +1,18 @@
 <?php $this->layout('master', ['title' => $title]) ?>
 
 <h1>User</h1>
+<?php echo flash('created') ?>
 
 <form action="/user/update" method="post">
-    <?php echo getToken(); ?>
+    <?php echo getToken() ?>
     <input type="text" name="firstName" id="firstName" value="Railton">
+    <?php echo flash('firstName') ?>
     <input type="text" name="lastName" id="lastName" value="Leal">
+    <?php echo flash('lastName') ?>
     <input type="email" name="email" id="email" value="railtonleal98@gmail.com">
+    <?php echo flash('email') ?>
     <input type="password" name="password" id="password" value="123">
+    <?php echo flash('password') ?>
 
     <button type="submit">Salvar</button>
 </form>
